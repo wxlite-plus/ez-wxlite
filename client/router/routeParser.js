d0a0e7e427a5c18e79df33c3823654ab63b8dc6a
Jack Lo <jack-lo@foxmail.com>
2018-12-21 19:29:27 +0800

    add:路由解析方式优化

--- client/router/routeParser.js
+++ client/router/routeParser.js
@@ -1,17 +1,41 @@
 const routes = require('./routes.js');
 
+const routeMap = {};
+
+Object.keys(routes).map((key) => {
+  const route = routes[key];
+  routeMap[key] = route;
+  if (route.path) {
+    routeMap[route.path] = route;
+  }
+  return key;
+});
+
+/**
+ * 路由名称转路径
+ * @param {string} name 路由名
+ */
+function name2path(name) {
+  return name.replace(/\./g, '/').replace(/\B([A-Z])/g, '_$1').toLowerCase();
+}
+
+/**
+ * 路由解析
+ * @param {string} name 路由名
+ */
 function parser(name) {
   if (!name) {
     throw new Error('路由名字不能为空');
   }
-  const stack = name.split('.');
-  stack.unshift({ inferior: routes });
-  try {
-    const res = stack.reduce((pV, cV) => pV && pV.inferior && pV.inferior[cV]);
-    return res;
-  } catch (err) {
-    throw err;
+  let route = routeMap[name];
+  if (!route) {
+    const path = `/pages/${name2path(name)}/index`;
+    route = routeMap[path] || {
+      type: 'page',
+      path,
+    };
   }
+  return route;
 }
 
 module.exports = parser;
